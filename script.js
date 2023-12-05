a59452eebc59cda4e9377a026b89f9598044d73e
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-12-05 20:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+//your JS code here. If required.
+//your JS code here. If required.
+const squares = document.querySelectorAll('.square')
+
+squares.forEach((el,i)=>{
+	el.addEventListener('mouseover',e=>{
+		squares.forEach(el=>{
+			if(el!=e.target){
+				el.style.backgroundColor = "rgb(111, 78, 55)"
+			}
+		})
+	})
+	el.addEventListener('mouseout',e=>{
+		squares.forEach(el=>{
+			if(el!=e.target){
+				el.style.backgroundColor = "rgb(230, 230, 250)"
+			}
+		})
+	})
+})
